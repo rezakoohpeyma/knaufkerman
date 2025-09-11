6493b3ea6f8b8a2cc7388d329ceb480edd4c4779
rezakoohpeyma <rezakoohpeyma2@gmail.com>
2025-09-11 17:13:08 +0330

    create a file with server

---
+++ server.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+// server.js
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // تمام روت‌ها رو بده دست Next.js
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Server ready on http://localhost:${port}`);
+  });
+});
